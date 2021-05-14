55bbb6b5dc31dfa9c40c1eacff3b3a8abc722d3b
Vignesh B S <55937928+vignesh-bhat1999@users.noreply.github.com>
2021-05-14 11:04:55 +0530

    added review changes

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,7 +1,7 @@
-export const NotFound = () => {
+export default function NotFound() {
   return (
     <div style={{ margin: "auto", textAlign: "center" }}>
       <h2>Page Not Found</h2>
     </div>
   );
-};
+}
